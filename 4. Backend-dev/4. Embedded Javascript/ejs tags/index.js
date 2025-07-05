2a096183676e5e45d0ee448cc241521dfbd17e95
DevxPiyush <devxpiyush16.02@gmail.com>
2025-07-05 22:14:45 +0530

    Web Dev

---
+++ 4. Backend-dev/4. Embedded Javascript/ejs tags/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import ejs from "ejs";
+
+const app = express ();
+const port = 3000;
+
+app.get("/", (req,res)=>{
+    const today = new Date();
+    const data = {
+        title : "EJS TAG",
+        seconds : today.getSeconds(),
+        items : ["apple","banana","cherry"],
+        htmlcontent : "This is a emphasized  line",
+    };
+    res.render("index.ejs", data);
+});
+
+
+app.listen(port, (req,res)=>{
+    console.log(`app running on port ${port}`);
+})
---
+++ 4. Backend-dev/4. Embedded Javascript/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req,res)=>{
+    const today = new Date("June 26, 2020 11:00:35"); // either live blank aur keep the date yourself as per the need
+    const day = today.getDay();
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    // console.log("day");
+    let type = "weekday"
+    let tip = `Hey! it is the ${daysOfWeek[day]}, you gotta work hard`;
+    if (day === 0 || day === 6 ) {
+        type = "weekend";
+        tip = `Hey! it is the ${daysOfWeek[day]}, you gotta enjoy the day`;
+    }
+    res.render("index.ejs", {
+    dayType :type,
+    advice : tip,
+   });
+});
+
+app.listen(port, (req,res)=>{
+    console.log(`server running on port ${port}`);
+});
